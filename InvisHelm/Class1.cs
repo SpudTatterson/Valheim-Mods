7dfd3b28e44d5e47969e416ea62a13d534b14038
SpudTatterson <tchcjhui123@gmail.com>
2022-05-20 21:21:30 +0300

    Added Mods

---
+++ InvisHelm/Class1.cs
@@ -0,0 +1,112 @@
+﻿using System.IO;
+using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using UnityEngine;
+
+
+namespace invishelm
+{
+	[BepInPlugin("Spuds.invishelm", "invishelm", "1.1.3")]
+	public class invishelm : BaseUnityPlugin
+	{
+		public static ConfigEntry<bool> ConsoleComments;
+		public static ConfigEntry<KeyCode> hidehelmet;
+		public void Awake()
+		{
+			hidehelmet = Config.Bind<KeyCode>("General", "Hide Key", KeyCode.H, "The key is used to hide the helmet");
+			ConsoleComments = Config.Bind<bool>("General", "Show in console", false, "Show in console if you are in hide or show mode");
+
+			var harmony = new Harmony("Spuds.invishelm");
+			harmony.PatchAll();
+		}
+
+
+
+		public static bool On = true;
+
+		[HarmonyPatch]
+		public static class Patches
+		{
+			[HarmonyPrefix]
+			[HarmonyPatch(typeof(VisEquipment), "SetHelmetEquiped")]
+			public static bool VisEquipment_SetHelmetEquiped(ref VisEquipment __instance, int hash, int hairHash)
+			{
+				if (hash == 0)
+				{
+					return true;
+				}
+
+				if (hash == 703889544)
+				{
+
+					return true;
+				}
+				//player.m_helmetItem.m_shared.m_helmetHideHair = false;
+				if (Input.GetKeyDown(invishelm.hidehelmet.Value) && On == true)
+				{
+
+					if (__instance.m_helmetItemInstance)
+					{
+						UnityEngine.Object.Destroy(__instance.m_helmetItemInstance);
+					}
+					__instance.m_helmetItemInstance = __instance.AttachItem(hash, 0, __instance.m_helmet, true);
+
+					On = false;
+					if (ConsoleComments.Value)
+					{
+						Console.instance.Print("Show helmet mode");
+					}
+
+					return false;//
+
+				}
+				if (Input.GetKeyDown(invishelm.hidehelmet.Value) && On == false)
+				{
+
+					UnityEngine.Object.Destroy(__instance.m_helmetItemInstance);
+					On = true;
+					if (ConsoleComments.Value)
+					{
+						Console.instance.Print("Hide helmet mode");
+					}
+
+					return false;//
+				}
+
+				return false;//
+			}
+			[HarmonyPrefix]
+			[HarmonyPatch(typeof(VisEquipment), "SetHairEquiped")]
+			public static bool VisEquipment_SetHairEquiped(ref VisEquipment __instance, int hash)
+			{
+
+				if (hash == 703889544)
+				{
+					return true;
+				}
+				if (hash == 0)
+				{
+					return true;
+				}
+
+				if (Input.GetKeyDown(invishelm.hidehelmet.Value) && On == false)
+				{
+
+					UnityEngine.Object.Destroy(__instance.m_hairItemInstance);
+					return true;
+				}
+				if (Input.GetKeyDown(invishelm.hidehelmet.Value) && On == true)
+				{
+
+					if (__instance.m_hairItemInstance)
+					{
+						UnityEngine.Object.Destroy(__instance.m_hairItemInstance);
+					}
+					__instance.m_hairItemInstance = __instance.AttachItem(hash, 0, __instance.m_helmet, true);
+				}
+				return true;
+			}
+		}
+	}
+}
---
+++ invisArmor/Class1.cs
@@ -0,0 +1,216 @@
+﻿using System.IO;
+using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using UnityEngine;
+
+
+namespace invisarmor
+{
+    [BepInPlugin("Spuds.invisarmor", "InvisArmor", "1.3.2")]
+    public class Config : BaseUnityPlugin
+    {
+
+        public void Awake()
+        {
+            hidearmor = Config.Bind<KeyCode>("General", "Hide armor", KeyCode.H, "The key is used to hide the chestplate and cape");
+            hidecape = Config.Bind<bool>("General", "Hide Cape", true, "Hides cape");
+            hidechest = Config.Bind<bool>("General", "Hide Chestplate", true, "Hides Chestplate");
+            hideutil = Config.Bind<bool>("General", "Hide Utility", true, "Hides Utility(belt)");
+            hidepants = Config.Bind<bool>("General", "Hide Pants", false, "Hides Pants");
+
+            var harmony = new Harmony("Spuds.InvisArmor");
+            harmony.PatchAll();
+        }
+
+
+
+        // public static ConfigFile configFile = new ConfigFile(Path.Combine(Paths.ConfigPath, "InvisArmor.cfg"), true);
+        public static ConfigEntry<KeyCode> hidearmor;
+        public static ConfigEntry<bool> hidecape;
+        public static ConfigEntry<bool> hidechest;
+        public static ConfigEntry<bool> hideutil;
+        public static ConfigEntry<bool> hidepants;
+
+
+        //public static bool CapeOn = true;
+        public static bool On = true;
+
+
+        [HarmonyPatch]
+        public static class Patches
+        {
+            [HarmonyPrefix]
+            [HarmonyPatch(typeof(VisEquipment), "SetChestEquiped")]
+            public static bool VisEquipment_SetChestEquiped(ref VisEquipment __instance, int hash)
+            {
+
+                if (hash == 0)
+                {
+                    return true;
+                }
+
+                if (Input.GetKeyDown(invisarmor.Config.hidearmor.Value) && On == true)
+                {
+                    if (hidechest.Value == true)
+                    {
+                        GameObject itemPrefab = ObjectDB.instance.GetItemPrefab(hash);
+                        if (itemPrefab == null)
+                        {
+                            ZLog.Log("Missing chest item " + hash);
+                            return true;
+                        }
+                        ItemDrop component = itemPrefab.GetComponent<ItemDrop>();
+                        if (component.m_itemData.m_shared.m_armorMaterial)
+                        {
+                            __instance.m_bodyModel.material.SetTexture("_ChestTex", component.m_itemData.m_shared.m_armorMaterial.GetTexture("_ChestTex"));
+                            __instance.m_bodyModel.material.SetTexture("_ChestBumpMap", component.m_itemData.m_shared.m_armorMaterial.GetTexture("_ChestBumpMap"));
+                            __instance.m_bodyModel.material.SetTexture("_ChestMetal", component.m_itemData.m_shared.m_armorMaterial.GetTexture("_ChestMetal"));
+                        }
+                        __instance.m_chestItemInstances = __instance.AttachArmor(hash, -1);
+                    }
+                    On = false;
+                    return true;
+                }
+                if (Input.GetKeyDown(invisarmor.Config.hidearmor.Value) && On == false)
+                {
+                    if (hidechest.Value == true)
+                    {
+                        foreach (GameObject gameObject in __instance.m_chestItemInstances)
+                        {
+                            if (__instance.m_lodGroup)
+                            {
+                                Utils.RemoveFromLodgroup(__instance.m_lodGroup, gameObject);
+                            }
+                            UnityEngine.Object.Destroy(gameObject);
+                        }
+                        __instance.m_bodyModel.material.SetTexture("_ChestTex", __instance.m_emptyBodyTexture);
+                        __instance.m_bodyModel.material.SetTexture("_ChestBumpMap", null);
+                        __instance.m_bodyModel.material.SetTexture("_ChestMetal", null);
+                    }
+                    On = true;
+                    return false;
+                }
+
+                return !hidechest.Value;
+            }
+            [HarmonyPrefix]
+            [HarmonyPatch(typeof(VisEquipment), "SetShoulderEquiped")]
+            public static bool VisEquipment_SetShoulderEquiped(ref VisEquipment __instance, int hash, int variant)
+            {
+
+                if (hash == 0)
+                {
+                    return true;
+                }
+                if (hidecape.Value == true)
+                {
+                    if (Input.GetKeyDown(hidearmor.Value) && On == true)
+                    {
+                        foreach (GameObject gameObject in __instance.m_shoulderItemInstances)
+                        {
+                            if (__instance.m_lodGroup)
+                            {
+                                Utils.RemoveFromLodgroup(__instance.m_lodGroup, gameObject);
+                            }
+                            UnityEngine.Object.Destroy(gameObject);
+                        }
+
+                        return false;
+                    }
+                    if (Input.GetKeyDown(hidearmor.Value) && On == false)
+                    {
+                        __instance.m_shoulderItemInstances = __instance.AttachArmor(hash, variant);
+
+                        return false;
+                    }
+                }
+                return !hidecape.Value;
+            }
+            [HarmonyPrefix]
+            [HarmonyPatch(typeof(VisEquipment), "SetUtilityEquiped")]
+            public static bool VisEquipment_SetUtilityEquiped(ref VisEquipment __instance, int hash)
+            {
+
+                if (hash == 0)
+                {
+
+                    return true;
+                }
+                if (hideutil.Value == true)
+                {
+                    if (Input.GetKeyDown(hidearmor.Value) && On == true)
+                    {
+                        foreach (GameObject gameObject in __instance.m_utilityItemInstances)
+                        {
+                            if (__instance.m_lodGroup)
+                            {
+                                Utils.RemoveFromLodgroup(__instance.m_lodGroup, gameObject);
+                            }
+                            UnityEngine.Object.Destroy(gameObject);
+                        }
+
+                        return false;
+                    }
+                    if (Input.GetKeyDown(hidearmor.Value) && On == false)
+                    {
+                        __instance.m_utilityItemInstances = __instance.AttachArmor(hash, -1);
+
+                        return false;
+                    }
+                }
+                return !hideutil.Value;
+            }
+            [HarmonyPrefix]
+            [HarmonyPatch(typeof(VisEquipment), "SetLegEquiped")]
+            public static bool VisEquipment_SetLegEquiped(ref VisEquipment __instance, int hash)
+            {
+                if (hash == 0)
+                {
+                    return true;
+                }
+                if (hidepants.Value == true)
+                {
+                    if (Input.GetKeyDown(hidearmor.Value) && On == true)
+                    {
+
+                        foreach (GameObject obj in __instance.m_legItemInstances)
+                        {
+                            UnityEngine.Object.Destroy(obj);
+                        }
+                        __instance.m_legItemInstances = null;
+                        __instance.m_bodyModel.material.SetTexture("_LegsTex", __instance.m_emptyBodyTexture);
+                        __instance.m_bodyModel.material.SetTexture("_LegsBumpMap", null);
+                        __instance.m_bodyModel.material.SetTexture("_LegsMetal", null);
+                        return false;
+                    }
+                    if (Input.GetKeyDown(hidearmor.Value) && On == false)
+                    {
+                        GameObject itemPrefab = ObjectDB.instance.GetItemPrefab(hash);
+                        if (itemPrefab == null)
+                        {
+                            ZLog.Log("Missing legs item " + hash);
+                            return true;
+                        }
+                        ItemDrop component = itemPrefab.GetComponent<ItemDrop>();
+                        if (component.m_itemData.m_shared.m_armorMaterial)
+                        {
+                            __instance.m_bodyModel.material.SetTexture("_LegsTex", component.m_itemData.m_shared.m_armorMaterial.GetTexture("_LegsTex"));
+                            __instance.m_bodyModel.material.SetTexture("_LegsBumpMap", component.m_itemData.m_shared.m_armorMaterial.GetTexture("_LegsBumpMap"));
+                            __instance.m_bodyModel.material.SetTexture("_LegsMetal", component.m_itemData.m_shared.m_armorMaterial.GetTexture("_LegsMetal"));
+                        }
+                        __instance.m_legItemInstances = __instance.AttachArmor(hash, -1);
+                        return false;
+                    }
+
+                }
+                return !hidepants.Value;
+            }
+        }
+
+
+
+    }
+}
+
+
